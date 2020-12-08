e8566e1998befac926790e5e63df4746a0f27356
Gopal_Disys_MBRHE <gopal.chinnadurai@disys.com>
2020-12-08 13:09:06 +0530

    Initial MyLib Proj Setupp

---
+++ MyLib/MyLib.h
@@ -0,0 +1,19 @@
+//
+//  MyLib.h
+//  MyLib
+//
+//  Created by Gopalakrishnan Chinnadurai on 08/12/20.
+//  Copyright © 2020 Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLib.
+FOUNDATION_EXPORT double MyLibVersionNumber;
+
+//! Project version string for MyLib.
+FOUNDATION_EXPORT const unsigned char MyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLib/PublicHeader.h>
+
+
---
+++ MyLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLibExample
+//
+//  Created by Gopalakrishnan Chinnadurai on 08/12/20.
+//  Copyright © 2020 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
